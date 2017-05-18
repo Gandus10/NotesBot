f237142c2e298311f36b5ec46d5373ce54c47110
laurent.gander <laurent.gander@he-arc.ch>
2017-05-18 12:14:00 +0200

    Debut de notes-bot

--- bot/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Travis bot package."""
-
-from .api import api  # noqa
-from .bot import bot  # noqa
-from .conf import URL, HOST, PORT, TRAVIS_CONFIG_URL  # noqa
-from .web import make_app  # noqa
