e869dd1f102fcd4787ea32000351ce5bb71e5832
Sylvain Renaud <sylvain.renaud@he-arc.ch>
2017-05-31 22:13:28 +0200

    Résolu sérialisation avec setstate et getstate.

--- datasv2.py
+++ datasv2.py
@@ -1,23 +1,37 @@
 import pickle
 from collections import defaultdict
 from collections.abc import Collection
-from datasv2 import Branch
+
+def dd_branch():
+	return Branch()
+
+def dd_module():
+	return Module()
+
 
 class Grade:
-	"""docstring for Grade"""
+	"""Represents a grade, with a weight."""
+
 	def __init__(self, value, weight=1):
 		self.value = value
 		self.weight = weight
 
 	def __repr__(self):
-		return "{self.grade} ({self.weight})".format(self=self)
+		return f"{self.grade} ({self.weight})"
 
 
 class Branch:
-	"""Represents a branch, contains a list of grades."""
+	"""Represents a branch, contains a list of grades. Also has a weight in the module."""
+
 	def __init__(self, weight=1):
 		self.weight = weight
 		self.grades = []
+
+	def __iadd__(self, values):
+		if not isinstance(values, Collection):
+			values = [values]
+		self.grades.append(Grade(*values))
+		return self
 
 	def average(self):
 		somme = 0
@@ -28,31 +42,29 @@ class Branch:
 		assert(diviseur > 0)
 		return round(somme/diviseur, 1)
 
-	def __iadd__(self, values):
-		if not isinstance(values, Collection):
-			values = [values]
-		self.grades.append(Grade(*values))
-		return self
-
-#	def add_grade(self, value, weight):
-#		self.grades.append(Grade(value, weight))
-#		return self.grades
-
 
 class Module:
 	"""Represents the module, contains list of branches."""
+
 	def __init__(self):
-		self.branches = defaultdict(Branch)
+		self.branches = defaultdict(dd_branch)
 
 	def __getattr__(self, name):
 		return self.branches[name]
 
 	def __iadd__(self, values):
+		"""Error, branch has no attribute name."""
 		if not isinstance(values, Collection):
 			values = [values]
 		branch = Branch(*values)
 		self.branches[branch.name] = branch
 		return self
+
+	def __getstate__(self):
+		return self.branches
+
+	def __setstate__(self, state):
+		self.branches = state
 
 	def average(self):
 		somme = 0
@@ -63,26 +75,13 @@ class Module:
 		assert(diviseur > 0)
 		return round(somme/diviseur,1)
 
-#	def get_Branch(self, branch_name):
-#		for branch in self.branches:
-#			if(branch.name == branch_name):
-#				return branch
-#		return None
-#
-#	def add_Branch(self, name_Branch, weight_branch):
-#		if self.get_branch(name_Branch) == None:
-#			self.branches.append(Branch(name_Branch, weight_branch))
-#			return self.branches[-1]
-#		else:
-#			print("Cours déjà existant")
-
 
 class User:
 	"""Represents the user, has a list of modules."""
 
 	def __init__(self, username):
 		self.username = username
-		self.modules = defaultdict(Module)
+		self.modules = defaultdict(dd_module)
 
 	def __iadd__(self, values):
 		if not isinstance(values, Collection):
@@ -101,44 +100,20 @@ class User:
 		with open(self.username + ".dat", "rb") as f:
 			modules = pickle.load(f)
 
-#	def get_module(self, module_name):
-#		for module in self.modules:
-#			if(module.name == module_name):
-#				return module
-#		return None
-#
-#	def add_module(self, name_module):
-#		if self.modules[name_module] == None:
-#			self.modules[name_module] = Module(name_module)
-#			return self.modules[name_module]
-#		else:
-#			print("Module déjà existant")
-	
 
 def load_user(name):
 	user = User(name);
 	user.load()
 	return user
 
-jean = User("jean")
+jean = load_user("jean")
+
+#mod = input("module")
+#cours = input("cours")
 
 jean.sciences.math += 4
 jean.sciences.analyse += 5
+
 print(jean.sciences.average(),"/", jean.sciences.math.average(),"/", jean.sciences.analyse.average())
 
 jean.save();
-
-jean2 = load_user("jean2")
-print(jean2.sciences.math.average())
-
-#jean.add_module("sciences").add_Branch("math", 1).add_Grade(5, 1)
-#
-#jean.save()
-#jean.load()
-#
-#print("Moyenne du cours:", jean.get_module("sciences").get_Branch("math").average())
-#print("Moyenne du module:", jean.get_module("sciences").average())
-#
-#jean.get_module("sciences").get_Branch("math").add_Grade(6, 0.5)
-#print("Moyenne du cours après ajout d'un 6 de weight 0.5:", 
-#		jean.get_module("sciences").get_Branch("math").average())
