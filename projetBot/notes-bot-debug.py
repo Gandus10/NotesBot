730a004c1e1bb63cfc48785b3a0c3a366f3c0a2b
Sylvain Renaud <sylvain.renaud@he-arc.ch>
2017-06-04 15:53:33 +0200

    Ajout d'un module pour debugging de notes-bot.py.

--- projetBot/notes-bot-debug.py
+++ projetBot/notes-bot-debug.py
@@ -1,13 +1,48 @@
 """Bot exemple qui retourne les moyennes du cours donnés."""
-
+import argparse
 import asyncio
 import json
+import logging
 import os
+import sys
+import warnings
 import zlib
 
 import aiohttp
 
 from dataprocess import load_user
+
+parser = argparse.ArgumentParser('debugging asyncio')
+parser.add_argument(
+    '-v',
+    dest='verbose',
+    default=False,
+    action='store_true',
+)
+args = parser.parse_args()
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)7s: %(message)s',
+    stream=sys.stderr,
+)
+LOG = logging.getLogger('')
+
+loop = asyncio.get_event_loop()
+
+if args.verbose:
+    LOG.info('enabling debugging')
+
+    # Enable debugging
+    loop.set_debug(True)
+
+    # Make the threshold for "slow" tasks very very small for
+    # illustration. The default is 0.1, or 100 milliseconds.
+    loop.slow_callback_duration = 0.001
+
+    # Report all mistakes managing asynchronous resources.
+    warnings.simplefilter('always', ResourceWarning)
+
 
 # Jupyter hack pour recréer une boucle.
 # Pas nécessaire hors de Jupyter
@@ -33,6 +68,7 @@ HEADERS = {
 
 async def api_call(path, method="GET", **kwargs):
     """Effectue une  requête sur l'API REST de Discord."""
+    LOG.info('api_call starting')
     default = {"headers": HEADERS}
     kwargs = dict(default, **kwargs)
     with aiohttp.ClientSession() as session:
@@ -47,14 +83,17 @@ async def api_call(path, method="GET", **kwargs):
                 raise AssertionError(
                     f"{response.status} {response.reason}"
                     f" was unexpected.\n{body}")
+    LOG.info('api_call finished')
 
 
 async def send_message(recipient_id, content):
     """Envoie un message à l'utilisateur donné."""
+    LOG.info('send message started')
     channel = await api_call("/users/@me/channels",
                              "POST", json={"recipient_id": recipient_id})
     return await api_call(f"/channels/{channel['id']}/messages",
                           "POST", json={"content": content})
+
 
 # Pas très joli, mais ça le fait.
 last_sequence = None
@@ -62,15 +101,19 @@ last_sequence = None
 
 async def heartbeat(ws, interval):
     """Tâche qui informe Discord de notre présence."""
+    LOG.info('heartbeat starting')
     while True:
         await asyncio.sleep(interval / 1000)
         print("> Heartbeat")
         await ws.send_json({'op': 1,  # Heartbeat
                             'd': last_sequence})
 
+    LOG.info('heartbeat finished')
+
 
 async def identify(ws):
     """Tâche qui identifie le bot à la Web Socket (indispensable)."""
+    LOG.info('indentify starting')
     await ws.send_json({'op': 2,  # Identify
                         'd': {'token': TOKEN,
                               'properties': {},
@@ -79,14 +122,19 @@ async def identify(ws):
                               # à zlib, pas nécessaire.
                               'large_threshold': 250}})
 
+    LOG.info('indentify finished')
+
 
 async def start(ws):
     """Lance le bot sur l'adresse Web Socket donnée."""
     global last_sequence
+    LOG.info('start start')
     # global est nécessaire pour modifier la variable
     with aiohttp.ClientSession() as session:
         async with session.ws_connect(f"{ws}?v=5&encoding=json") as ws:
             async for msg in ws:
+                print(msg)
+                print(ws)
                 if msg.tp == aiohttp.WSMsgType.TEXT:
                     data = json.loads(msg.data)
                 elif msg.tp == aiohttp.WSMsgType.BINARY:
@@ -105,6 +153,7 @@ async def start(ws):
                 elif data['op'] == 11:  # Heartbeat ACK
                     print("< Heartbeat ACK")
                 elif data['op'] == 0:  # Dispatch
+                    print("Dispatch")
                     last_sequence = data['s']
                     if data['t'] == "MESSAGE_CREATE":
 
@@ -124,14 +173,20 @@ async def start(ws):
                 else:
                     print("Unknown?", data)
 
+            print("Exit loop")
+
+    LOG.info('start finished')
+
 
 async def main():
     """Main method."""
+    LOG.info('main starting')
     response = await api_call('/gateway')
     await start(response['url'])
+    LOG.info('main finished')
 
 
-def message_received(data):
+async def message_received(data):
     """Process the received message. Send a response."""
     print(data['d'])
     if(data['d']['author']['username'] != "Bot-notes"):
@@ -209,7 +264,7 @@ help_msg = """Ajouter un cours avec :
     ```help ``` """
 
 # Lancer le programme.
-loop = asyncio.get_event_loop()
-loop.set_debug(True)
+
+LOG.info('entering event loop')
 loop.run_until_complete(main())
 loop.close()
